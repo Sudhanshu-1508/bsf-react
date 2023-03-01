c34f26b107bb0747f5121f30dd9070eb69415109
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-03-01 12:17:39 +0530

    .

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,15 +4,20 @@ import Hello from "./components/Hello";
 import Counter from "./components/Counter";
 import './App.css';
 import Message from "./components/Message";
+import FunctionClick from "./components/FunctionClick";
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
-     {/* <Message />
-      <Greet name= "Sudhanshu" heroName="Batman" />
-      <Greet name={"Kulkarni"} heroName="Superman"/>
-      <Hello></Hello> */}
-      <Counter />
+     <Message />
+      <Greet name= "Sudhanshu" heroName="123" />
+      <Greet name={"Kulkarni"} heroName="searfa"/>
+      <Hello></Hello> 
+      <FunctionClick />
+      <EventBind />
+      <ParentComponent />
     </div>
   );
 }
